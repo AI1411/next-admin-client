6259a9cddd570cdeef6e518b4f26e5b2896845b8
AI1411 <goethe0719@gmail.com>
2022-05-30 06:37:04 +0900

    impl register

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,8 +1,31 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import React from 'react';
+import {SubmitHandler, useForm} from "react-hook-form";
+import {useRouter} from "next/router";
+import axios from "axios";
+import {BASE_URL} from "../../../lib/utils/const";
+
+type RegisterParams = {
+  first_name: string;
+  last_name: string;
+  age: number;
+  email: string;
+  password: string;
+  password_confirmation: string;
+};
 
 const Register = () => {
+  const {register, handleSubmit, formState: {errors}} = useForm<RegisterParams>();
+  const router = useRouter();
+  const onSubmit: SubmitHandler<RegisterParams> = data => {
+    data.age = Number(data.age);
+    axios.post(`${BASE_URL}/auth/register`, data).then(() => {
+      return router.push('/users');
+    }).catch(err => {
+      alert(err);
+    })
+  };
   return (
     <div>
       <Head>
@@ -18,58 +41,77 @@ const Register = () => {
                  alt="Windster Logo"/>
             <span className="self-center text-2xl font-bold whitespace-nowrap">Windster</span>
           </a>
-
           <div className="bg-white shadow rounded-lg md:mt-0 w-full sm:max-w-screen-sm xl:p-0">
             <div className="p-6 sm:p-8 lg:p-16 space-y-8">
               <h2 className="text-2xl lg:text-3xl font-bold text-gray-900">
                 Sign in to platform
               </h2>
-              <form className="mt-8 space-y-6" action="#">
+              <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
                 <div>
                   <label htmlFor="email" className="text-sm font-medium text-gray-900 block mb-2">Your
                     First Name</label>
-                  <input type="text" name="email" id="email"
-                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
-                         placeholder="First Name"/>
+                  <input
+                    {...register('first_name', {required: true, maxLength: 64})}
+                    type="text"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                    placeholder="First Name"
+                  />
+                  {errors.first_name && <span className="text-xs italic text-red-500">名は必須です</span>}
                 </div>
                 <div>
                   <label htmlFor="email" className="text-sm font-medium text-gray-900 block mb-2">Your
                     Last Name</label>
-                  <input type="text" name="email" id="email"
-                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
-                         placeholder="Last Name"/>
+                  <input
+                    {...register('last_name', {required: true, maxLength: 64})}
+                    type="text"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                    placeholder="Last Name"
+                  />
+                  {errors.last_name && <span className="text-xs italic text-red-500">姓は必須です</span>}
                 </div>
                 <div>
                   <label htmlFor="email" className="text-sm font-medium text-gray-900 block mb-2">Your
                     Age</label>
-                  <input type="text" name="email" id="email"
-                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
-                         placeholder="37"/>
+                  <input
+                    {...register('age', {required: true, min: 18})}
+                    type="text"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                    placeholder="37"
+                  />
+                  {errors.age && <span className="text-xs italic text-red-500">年齢は必須です</span>}
                 </div>
                 <div>
                   <label htmlFor="email" className="text-sm font-medium text-gray-900 block mb-2">Your
                     email</label>
-                  <input type="email" name="email" id="email"
-                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
-                         placeholder="name@company.com"/>
+                  <input
+                    {...register('email', {required: true})}
+                    type="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                    placeholder="name@company.com"
+                  />
+                  {errors.email && <span className="text-xs italic text-red-500">メールアドレスは必須です</span>}
                 </div>
                 <div>
                   <label htmlFor="password" className="text-sm font-medium text-gray-900 block mb-2">Your
                     password</label>
-                  <input type="password" name="password" id="password" placeholder="••••••••"
-                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                  <input
+                    {...register('password', {required: true})}
+                    type="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
                   />
+                  {errors.password && <span className="text-xs italic text-red-500">パスワードは必須です</span>}
                 </div>
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input id="remember" aria-describedby="remember" name="remember" type="checkbox"
-                           className="bg-gray-50 border-gray-300 focus:ring-3 focus:ring-cyan-200 h-4 w-4 rounded"
-                    />
-                  </div>
-                  <div className="text-sm ml-3">
-                    <label htmlFor="remember" className="font-medium text-gray-900">Remember me</label>
-                  </div>
-                  <a href="#" className="text-sm text-teal-500 hover:underline ml-auto">Lost Password?</a>
+                <div>
+                  <label htmlFor="password" className="text-sm font-medium text-gray-900 block mb-2">Your
+                    password confirm</label>
+                  <input
+                    {...register('password_confirmation', {required: true})}
+                    type="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                  />
+                  {errors.password_confirmation && <span className="text-xs italic text-red-500">パスワード確認は必須です</span>}
                 </div>
                 <button type="submit"
                         className="text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-base px-5 py-3 w-full sm:w-auto text-center">Login
